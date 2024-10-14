47e6089af03a94707cce67ae2879b0c0fca0fcca
Sim <0doogadooga@gmail.com>
2024-10-15 01:17:44 +0900

    임심

--- LearnReactNative/App.tsx
+++ LearnReactNative/App.tsx
@@ -12,27 +12,36 @@ import {
   Text,
   View,
 } from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 
 
 
 
 function App(): React.JSX.Element {
-  console.log('sdf')
+  const value = useSharedValue(0)
 
   const onPressButton = useCallback(() => {
-
+    value.value = Math.random() * 100
   }, [])
+
+  const animStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: value.value }]
+    }
+  })
   return (
     <SafeAreaView style={{ flex: 1 }} >
       <View style={{ flex: 1, borderWidth: 1 }}>
         <Button title='박스 움직이기' onPress={onPressButton} />
         {/* <Text>ddidi</Text> */}
         <Animated.View
-          style={{
-            width: 50, height: 50, borderRadius: 6, backgroundColor: 'blue'
-          }}
+          style={[
+            {
+              width: 50, height: 50, borderRadius: 6, backgroundColor: 'blue'
+            },
+            animStyles
+          ]}
         />
       </View>
     </SafeAreaView>
